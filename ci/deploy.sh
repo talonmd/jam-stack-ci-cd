0d843c651a2a21b9234b922975c7769fe76c56dd
Talon Durrant <talonmdurrant@gmail.com>
2020-11-03 20:50:42 -0700

    [CI] add deploy script to execute on travis success

---
+++ ci/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# setting ENVs
+# if current build is from the git development branch the use the shared keys
+# export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID_SHARED}" # in shared environment
+
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+export BUCKET="${BUCKET}"
+export CLOUDFRONT_DISTRIBUTION_ID="${CLOUDFRONT_DISTRIBUTION_ID}"
+
+[[ -z "${AWS_ACCESS_KEY_ID}"]] && echo "AWS_ACCESS_KEY_ID is missing"
+
+# actual deployment
+aws s3 sync build/ "s3://${BUCKET}"
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --path "/*"
